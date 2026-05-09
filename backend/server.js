c23e27740d33148ba0198f3d0a2565f9b6a07fc8
nitintomar1111 <nitintomar874281@gmail.com>
2026-05-09 22:32:21 +0530

    Removed email timeout issue

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,6 @@ app.post("/submit-assessment", async (req, res) => {
 
     try{
 
-        // Get frontend data
         const {
             name,
             email,
@@ -60,8 +59,6 @@ app.post("/submit-assessment", async (req, res) => {
             feedbackMessage
         } = req.body;
 
-
-        // Basic validation
         if(
             !name ||
             !email ||
@@ -72,80 +69,24 @@ app.post("/submit-assessment", async (req, res) => {
             return res.status(400).json({
                 message: "Missing required fields."
             });
-
         }
 
-        await transporter.sendMail({
+        console.log("Assessment Submitted");
 
-    from: process.env.EMAIL_USER,
+        console.log(name);
+        console.log(email);
+        console.log(totalScore);
 
-    to: email,
-
-    subject: "Leadership Assessment Report",
-
-    html: `
-
-<div style="
-    font-family: Arial, Helvetica, sans-serif;
-    max-width: 700px;
-    margin: auto;
-    padding: 30px;
-    background: #f4f7fb;
-">
-
-    <div style="
-        background: white;
-        padding: 40px;
-        border-radius: 14px;
-        box-shadow: 0 8px 25px rgba(0,0,0,0.08);
-    ">
-
-        <h1 style="
-            color:#1f3c88;
-            margin-bottom:20px;
-        ">
-            Leadership Assessment Report
-        </h1>
-
-        <p>Hello <strong>${name}</strong>,</p>
-
-        <p>
-            Thank you for completing your leadership assessment.
-        </p>
-
-        <p>
-            <strong>Total Score:</strong> ${totalScore}
-        </p>
-
-        <p>
-            <strong>Leadership Level:</strong>
-            ${leadershipBand}
-        </p>
-
-        <p>
-            <strong>Feedback:</strong>
-            ${feedbackMessage}
-        </p>
-
-    </div>
-
-</div>
-
-`
-});
-
-
-        // Success response
         res.status(200).json({
-    message: "Assessment submitted successfully."
-});
+            message: "Assessment submitted successfully."
+        });
 
     }catch(error){
 
-        console.error("EMAIL ERROR:", error);
+        console.error(error);
 
         res.status(500).json({
-            message: "Server error while sending email."
+            message: "Server error."
         });
     }
 });
